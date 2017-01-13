c7dbb37054f026e8443f9a83f0e7fa1fe1e5265a
tomahl <tomahl@tomahls-iMac.local>
2017-01-13 09:57:47 +0100

    Added ESlint task

---
+++ AAK.Web.Frontend/Type A/gulp/tasks/eslint.js
@@ -0,0 +1,28 @@
+/**
+ * ESLint
+ *
+ * A gulp plugin for processing files with ESLint
+ * The pluggable linting utility for JavaScript and JSX
+ *
+ * @see {@link https://www.npmjs.com/package/gulp-stylelint|gulp-stylelint}
+ * @see {@link http://eslint.org|ESLint}
+ */
+
+// Project Configuration
+var config		= require('../config.js');
+
+// Dependencies
+var gulp		= require('gulp');
+var eslint		= require('gulp-eslint');
+
+gulp.task('eslint', function() {
+
+	var options = {
+		rules: {
+			'no-extra-semi': 2
+        }
+	};
+
+	return gulp.src(config.SOURCE_SCRIPTS_PATH + '**/*')
+		.pipe(eslint(options));
+});
